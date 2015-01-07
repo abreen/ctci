46bca036fad47c7966cdbaeef647346048f812ab
Alexander Breen <alexander.breen@gmail.com>
2015-01-06 20:49:28 -0500

    Fixed 5.1, added book solution

--- 5/5.1/insertbits.py
+++ 5/5.1/insertbits.py
@@ -10,22 +10,53 @@
 def insert_into(n, m, i, j):
     # need sequence of j - i + 1 ones
     mask = 0
-    for i in range(j - i + 1):
-        mask |= 1 << i
+    for x in range(j - i + 1):
+        mask |= 1 << x
 
-    # take the desired bits from m
-    m &= mask
-
-    # shift m and mask so we start at i
+    # make mask start at i
     mask = mask << i
+
+    # clear relevant sequence of bits in n
+    n &= ~mask
+
+    # shift m into place
     m = m << i
 
     # inverting the mask here keeps the rest of n
-    solution = (n & ~mask) | m
+    solution = n | (m & mask)
 
     # we return both the value of the solution and a string, for ease
     return solution, bin(solution)
 
+N, M, I, J = 0b10000000, 0b101, 0, 2
+print(insert_into(N, M, I, J))
+
 # My guess is that there's another clever way to get j - i + 1 many
 # ones by finding the smallest power of 2 greater than that sum and
 # subtracting one. But I'm not certain it would be more efficient...
+
+# Note: the book's solution is cooler than that (I'm paraphrasing here,
+# and converting to Python):
+
+def insert_into(n, m, i, j):
+    ones = 0xffffffff
+
+    # we're pushing the ones left so that the first zero is at j
+    left = ones << (j + 1)
+
+    # right should have its first i bits set to 1
+    right = (1 << i) - 1
+
+    # e.g., 11100001 (for j = 5, i = 1)
+    mask = left | right
+
+    # clear bits to make way for n
+    n &= mask
+
+    # inverting the mask here keeps the rest of n
+    solution = n | (m << i)
+
+    # we return both the value of the solution and a string, for ease
+    return solution, bin(solution)
+
+print(insert_into(N, M, I, J))
